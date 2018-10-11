6f8d3e657fea5e6fcddbbcab2daff64f676f9071
20171105142 <921666333@qq.com>
2018-10-11 09:59:04 +0800

    显示运算符

--- caculaterDisplay/caculaterDisplay/ViewController.swift
+++ caculaterDisplay/caculaterDisplay/ViewController.swift
@@ -11,27 +11,43 @@ import UIKit
 class ViewController: UIViewController {
     
     var temp = 0
-
+    var sum = 0
     @IBOutlet weak var caculateDisplay: UILabel!
+    
+    @IBOutlet weak var caculateDisplay1: UILabel!
+    
     
     @IBAction func Button0(_ sender: Any) {
         caculateDisplay.text = caculateDisplay.text!+"0"
+        
     }
     @IBAction func Button1(_ sender: Any) {
         caculateDisplay.text = caculateDisplay.text!+"1"
+        caculateDisplay1.text = caculateDisplay1.text!+"1"
     }
     @IBAction func Button7(_ sender: Any) {
         caculateDisplay.text = caculateDisplay.text!+"7"
+        caculateDisplay1.text = caculateDisplay1.text!+"7"
+    }
+    @IBAction func clear(_ sender: Any) {
+        caculateDisplay.text = ""
+        caculateDisplay1.text = ""
     }
     @IBAction func buttonAdd(_ sender: Any) {
-        //caculateDisplay.text = caculateDisplay.text!+"+"
+        //temp=Int(caculateDisplay.text!)!
+     
         temp=Int(caculateDisplay.text!)!
+        
+        sum+=temp
+        caculateDisplay1.text = caculateDisplay1.text!+"+"
+        
         caculateDisplay.text = ""
     }
     @IBAction func buttonCaculator(_ sender: Any) {
-        var sum = 0
-        sum = temp + Int(caculateDisplay.text!)!
+        
+        sum = sum + Int(caculateDisplay.text!)!
         caculateDisplay.text = "\(sum)"
+        caculateDisplay1.text = caculateDisplay.text
     }
     override func viewDidLoad() {
         super.viewDidLoad()
