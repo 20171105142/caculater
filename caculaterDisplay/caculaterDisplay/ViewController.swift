3cf0663936397644c72d77de0c92fadc02ea1fe0
20171105142 <921666333@qq.com>
2018-10-12 09:53:35 +0800

    实现基本运算

--- caculaterDisplay/caculaterDisplay/ViewController.swift
+++ caculaterDisplay/caculaterDisplay/ViewController.swift
@@ -12,42 +12,72 @@ class ViewController: UIViewController {
     
     var temp = 0
     var sum = 0
+    var val = ""
     @IBOutlet weak var caculateDisplay: UILabel!
-    
-    @IBOutlet weak var caculateDisplay1: UILabel!
-    
-    
     @IBAction func Button0(_ sender: Any) {
         caculateDisplay.text = caculateDisplay.text!+"0"
         
     }
     @IBAction func Button1(_ sender: Any) {
         caculateDisplay.text = caculateDisplay.text!+"1"
-        caculateDisplay1.text = caculateDisplay1.text!+"1"
+        //caculateDisplay1.text = caculateDisplay1.text!+"1"
+    }
+    @IBAction func Button2(_ sender: Any) {
+        caculateDisplay.text = caculateDisplay.text!+"2"
+    }
+    @IBAction func Button3(_ sender: Any) {
+        caculateDisplay.text = caculateDisplay.text!+"3"
     }
     @IBAction func Button7(_ sender: Any) {
         caculateDisplay.text = caculateDisplay.text!+"7"
-        caculateDisplay1.text = caculateDisplay1.text!+"7"
+        //caculateDisplay1.text = caculateDisplay1.text!+"7"
+    }
+    @IBAction func buttonDot(_ sender: Any) {
+        caculateDisplay.text = caculateDisplay.text!+"."
     }
     @IBAction func clear(_ sender: Any) {
         caculateDisplay.text = ""
-        caculateDisplay1.text = ""
+        //caculateDisplay1.text = ""
     }
-    @IBAction func buttonAdd(_ sender: Any) {
-        //temp=Int(caculateDisplay.text!)!
-     
+    @IBAction func buttonAdd(_ sender: Any) {//加法
         temp=Int(caculateDisplay.text!)!
+        val = "+"
         
-        sum+=temp
-        caculateDisplay1.text = caculateDisplay1.text!+"+"
-        
+        //caculateDisplay1.text = caculateDisplay1.text!+"+"
         caculateDisplay.text = ""
     }
-    @IBAction func buttonCaculator(_ sender: Any) {
+    @IBAction func buttonSub(_ sender: Any) {//减法
+        temp=Int(caculateDisplay.text!)!
+        val = "-"
+        caculateDisplay.text = ""
+    }
+    @IBAction func buttonMul(_ sender: Any) {//乘法
+        temp=Int(caculateDisplay.text!)!
+        val = "*"
+        caculateDisplay.text = ""
+    }
+    @IBAction func buttonDiv(_ sender: Any) {//除法
+        temp=Int(caculateDisplay.text!)!
+        val = "/"
+        caculateDisplay.text = ""
+    }
+    @IBAction func buttonCaculator(_ sender: Any) {//等于
+        switch val {
+        case "+":
+            sum = temp + Int(caculateDisplay.text!)!
+        case "-":
+            sum = temp - Int(caculateDisplay.text!)!
+        case "*":
+            sum = temp * Int(caculateDisplay.text!)!
+        case "/":
+            sum = temp / Int(caculateDisplay.text!)!
+        default :
+            print("error")
+        }
         
-        sum = sum + Int(caculateDisplay.text!)!
+        //sum = sum + Int(caculateDisplay.text!)!
         caculateDisplay.text = "\(sum)"
-        caculateDisplay1.text = caculateDisplay.text
+        //caculateDisplay1.text = caculateDisplay.text
     }
     override func viewDidLoad() {
         super.viewDidLoad()
