c44a62efae62a079e57aa1c52373fa8a2b10847c
20171105142 <921666333@qq.com>
2018-10-19 09:01:56 +0800

    每次计算小数点每只能输入一次，计算器初始值为0

--- caculaterDisplay/caculaterDisplay/ViewController.swift
+++ caculaterDisplay/caculaterDisplay/ViewController.swift
@@ -13,55 +13,97 @@ class ViewController: UIViewController {
     var temp = 0
     var sum = 0
     var val = ""
+   
     @IBOutlet weak var caculateDisplay: UILabel!
+    var num:Bool = true
+    
     @IBAction func Button0(_ sender: Any) {
-        caculateDisplay.text = caculateDisplay.text!+"0"
+        if caculateDisplay.text == "0"{
+        //caculateDisplay.text = caculateDisplay.text!+"0"
+        }else{
+            caculateDisplay.text = caculateDisplay.text!+"0"
+        }
         
     }
     @IBAction func Button1(_ sender: Any) {
-        caculateDisplay.text = caculateDisplay.text!+"1"
+        if caculateDisplay.text == "0"{
+        caculateDisplay.text = "1"
+        }else{
+            caculateDisplay.text = caculateDisplay.text!+"1"
+        }
         //caculateDisplay1.text = caculateDisplay1.text!+"1"
     }
     @IBAction func Button2(_ sender: Any) {
-        caculateDisplay.text = caculateDisplay.text!+"2"
+        if caculateDisplay.text == "0"{
+            caculateDisplay.text = "2"
+        }else{
+            caculateDisplay.text = caculateDisplay.text!+"2"
+        }
     }
     @IBAction func Button3(_ sender: Any) {
-        caculateDisplay.text = caculateDisplay.text!+"3"
+        if caculateDisplay.text == "0"{
+            caculateDisplay.text = "3"
+        }else{
+            caculateDisplay.text = caculateDisplay.text!+"3"
+        }
     }
     @IBAction func Button7(_ sender: Any) {
-        caculateDisplay.text = caculateDisplay.text!+"7"
+        if caculateDisplay.text == "0"{
+            caculateDisplay.text = "7"
+        }else{
+            caculateDisplay.text = caculateDisplay.text!+"7"
+        }
         //caculateDisplay1.text = caculateDisplay1.text!+"7"
     }
     @IBAction func buttonDot(_ sender: Any) {
-        caculateDisplay.text = caculateDisplay.text!+"."
+        
+        if caculateDisplay.text == "" && num{
+            caculateDisplay.text = "0."
+        }else if num {
+            caculateDisplay.text = caculateDisplay.text!+"."
+        }
+        num = false
+      
     }
     @IBAction func clear(_ sender: Any) {
-        caculateDisplay.text = ""
+        caculateDisplay.text = "0"
+        num = true
         //caculateDisplay1.text = ""
     }
     @IBAction func buttonAdd(_ sender: Any) {//加法
-        temp=Int(caculateDisplay.text!)!
-        val = "+"
+        
+            temp=Int(caculateDisplay.text!)!
+            val = "+"
+            caculateDisplay.text = ""
         
         //caculateDisplay1.text = caculateDisplay1.text!+"+"
-        caculateDisplay.text = ""
+        
     }
     @IBAction func buttonSub(_ sender: Any) {//减法
-        temp=Int(caculateDisplay.text!)!
-        val = "-"
-        caculateDisplay.text = ""
+            temp=Int(caculateDisplay.text!)!
+            val = "-"
+            caculateDisplay.text = ""
+       
+        
     }
     @IBAction func buttonMul(_ sender: Any) {//乘法
-        temp=Int(caculateDisplay.text!)!
-        val = "*"
-        caculateDisplay.text = ""
+        
+            temp=Int(caculateDisplay.text!)!
+            val = "*"
+            caculateDisplay.text = ""
+       
     }
     @IBAction func buttonDiv(_ sender: Any) {//除法
-        temp=Int(caculateDisplay.text!)!
-        val = "/"
-        caculateDisplay.text = ""
+        
+            temp=Int(caculateDisplay.text!)!
+            val = "/"
+            caculateDisplay.text = ""
+        
+
     }
     @IBAction func buttonCaculator(_ sender: Any) {//等于
+        
+        
         switch val {
         case "+":
             sum = temp + Int(caculateDisplay.text!)!
@@ -72,8 +114,8 @@ class ViewController: UIViewController {
         case "/":
             sum = temp / Int(caculateDisplay.text!)!
         default :
-            print("error")
-        }
+            break
+            }
         
         //sum = sum + Int(caculateDisplay.text!)!
         caculateDisplay.text = "\(sum)"
